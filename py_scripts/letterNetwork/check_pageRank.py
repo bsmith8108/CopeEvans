348247999272e16459e8d3cce2fb6264ac56fcca
Brandon Smith <bsmith8108@gmail.com>
2014-06-03 15:54:03 -0400

    started working with the mapbox.js, but Google stopped me from querying for more locations when I wasn't quite finished with the algorithm. Need to do that tomorrow.

--- py_scripts/letterNetwork/check_pageRank.py
+++ py_scripts/letterNetwork/check_pageRank.py
@@ -2,11 +2,18 @@ import get_data as gd
 
 list_of_dicts = gd.get_data_list_of_dicts()
 
+list_of_dicts = sorted(list_of_dicts, key=lambda k : k["Date"])
+
 counter = 0
 for entry in list_of_dicts:
     creator = entry["Creator"].replace(" ", "")
     recipient = entry["Recipient"].replace(" ", "")
-    if creator == "RachelReeveCopeEvans:1850-1939" or recipient == "RachelReeveCopeEvans:1850-1939":
+    if (creator == "AnnaStewardsonBrownCope:1822-1916" or recipient == "AnnaStewardsonBrownCope:1822-1916") and len(entry["Transcript"]) >= 10:
 	counter += 1
+	print "\n-------------------------------------------------------"
+	transcript = entry["Transcript"].replace("<br>","\n")
+	transcript = transcript.replace("COMMA",",")
+	print transcript
+	print "\n------------------------------------------------------"
 
 print counter
---
+++ py_scripts/letterNetwork/get_location_network.py
@@ -0,0 +1,126 @@
+import get_data as gd
+import sys
+
+
+# Now that we have the list of people connected with who they sent
+# a letter to, we can calculate PageRank
+# Parts of code taken from Brandon Smith's kdtree.py file
+class Point:
+    def __init__(self, person):
+	self.name = person[0]
+	self.my_id = person[1]
+	self.pagerank = 0
+    
+    def __str__(self):
+	return str(self.my_id)
+    
+    def __repr__(self):
+	return str(self.my_id)
+
+    def getId(self):
+	return int(self.my_id)
+    
+    def getName(self):
+	return self.name
+
+    def setPageRank(self, val):
+	self.pagerank = val
+
+def make_points(people):
+    points = []
+    counter = 0
+    for person in people:
+        temp = [person,counter]
+	temp_point = Point(temp)
+	points.append(temp_point)
+	counter += 1
+
+    return points
+
+def pageRank(nn_list, points):
+    pageRanks = []
+    newPageRanks = []
+    InNodesList = []
+    N = len(points)
+    starter = 1.0/N
+    oldPageRanks = []
+    d = .85
+    constant = (1.0-d)/N
+
+    # initialize the page ranks list to the same initial values
+    for x in range(len(points)+1):
+	pageRanks.append(starter)
+	oldPageRanks.append(0.0)
+	InNodesList.append([])
+    
+    # need to find what nodes are pointing to what other nodes
+    # once I have this, it will be a constant time lookup to find
+    # In(u)
+    for entry in nn_list:
+	for item in entry["target"]:
+	    InNodesList[item.getId()].append(entry["source"])
+    """
+    Code from original PageRank function, modified for my use above
+    for entry in range(len(nn_list)):
+	for item in nn_list[entry]["target"]:
+	    InNodesList[item.getId()].append(points[entry])
+	    InNodesList[entry].append(item)
+    """
+    counter = 0
+    while not (oldPageRanks == pageRanks) and counter <=100:
+	counter += 1
+	for i in range(len(points)):
+	    """
+	    Page Rank:
+		PageRank(point) = (1-d)/N + d*Sum(PR(v)/Out(v))
+		for all v coming into the point
+	    """
+	    final = constant
+	    summation = 0.0
+	    InPoints = InNodesList[i]
+	    for j in range(len(InPoints)):
+		PR_v = pageRanks[InPoints[j].getId()]
+		Out_v = len(InNodesList[InPoints[j].getId()])
+		if Out_v == 0:
+		    Out_v = len(points)-1
+		summation = summation + (PR_v/Out_v)
+	    summation = summation*d
+	    final = final + summation
+	    # This just makes sure it doesn't take forever to converge
+	    final = float('%.5f'%(final))
+	    newPageRanks.append(final)
+	oldPageRanks = pageRanks[:]
+	pageRanks = newPageRanks[:]
+	newPageRanks = []
+
+    return pageRanks
+
+def find_point(name, points):
+    for p in points:
+	if name == p.getName():
+	    return p
+    sys.exit("Could not find point")
+
+list_of_dicts = gd.get_data_list_of_dicts()
+
+
+"""
+page_ranks = pageRank(creator_recipient_list,people)
+
+json_string = "{\"nodes\":[\n"
+
+counter = 0
+for person in people:
+    json_string += "{\"name\":\""+person.getName().replace("\"","'")+"\", \"pageRank\":"+str(page_ranks[counter])+"},\n"
+    counter += 1
+
+json_string += "],\n\"links\": [\n"
+
+for item in creator_recipient_list:
+    for target in item["target"]:
+	json_string += "{\"source\":" + str(item["source"].getId()) + ", \"target\":" + str(target.getId()) + "},\n"
+    
+json_string += "]\n}"
+
+print json_string
+"""
