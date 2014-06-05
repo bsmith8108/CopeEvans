137089ab1fbe58322ccb4e758a82ccc76e7198b1
Brandon Smith <bsmith8108@gmail.com>
2014-06-05 16:28:33 -0400

    have one basic filter working for one category, now I just have to keep adding them

--- py_scripts/letterNetwork/drawMap.js
+++ py_scripts/letterNetwork/drawMap.js
@@ -2,6 +2,8 @@ var map = L.mapbox.map('map', 'mzarafonetis.idm8dak7')
     .setView([40,-40.50],4)
     .addControl(L.mapbox.geocoderControl('mzarafonetis.idm8dak7'));
 
+var line_info_list = [];
+
 d3.csv("partialCurrectLocations.csv", function(error, data) {
     d3.csv("letterTravels.csv", function(error, travel) {
 	console.log("travel",travel)
@@ -60,7 +62,8 @@ d3.csv("partialCurrectLocations.csv", function(error, data) {
 	    info_string = info_string + "Recipient: " + info.Recipient;
 	    info_string = info_string + "<br>Transcript: <br>" + info.Transcript.split("COMMA").join(",");
 	    info_list[line._leaflet_id] = info_string;
-	    
+	    line_info_list[line._leaflet_id] = info;
+
 	    line.on('click', function(e) {
 		openFancyBox(e.target._leaflet_id);
 	    });
@@ -68,8 +71,32 @@ d3.csv("partialCurrectLocations.csv", function(error, data) {
 	    counter++;
 	}
 	
+	var all_paths = $("path");
+	for (var k=0;k<all_paths.length; k++) {
+	    $(all_paths[k]).attr("id","letter"+(k+685).toString());
+	}
+
 	function openFancyBox(id) {
 	    $.fancybox.open(info_list[id]);
 	}
+
+	$(".filter-button").click(function(d) {
+	    var name = d.currentTarget.id;
+	    filterMap(name);
+	});
+
+	function filterMap(name) {
+	    var filterNames = {"age":"Age","gender":"Gender","family":"Creator","transcript":"Transcript","subject":"Subject"};
+	    
+	    var lines = $("path");
+	    for(var i=0; i<lines.length-1; i++) {
+		var id = parseInt(lines[i].id.substr(6));
+		var line_json =line_info_list[id];
+		if(!(line_json["Age of Author"] == "10-20")) {
+		    $(lines[i]).css("display","none");
+		}
+	    }
+	}
     });
 });
+
