c059c1f2e900fcb59944ba7ac62272b14963fc3e
Brandon Smith <bsmith8108@gmail.com>
2014-06-26 15:16:08 -0400

    new slide up style for the content pages

--- NewDesign/resources/js/page.js
+++ NewDesign/resources/js/page.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    
+    $("p.info").slideUp();
+    $(".content h2").click(function() {
+	$(this).next().toggleClass("selected")
+	console.log($(this).next())
+	if (!$(this).next().hasClass("selected")) {
+	    $(this).next().slideUp();
+	}
+	else {
+	    $(this).next().slideDown();
+	}
+    });
 });
---
+++ NewDesign/update.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+cp intro.html travel.html
+cp intro.html education.html
+cp intro.html philanthropy.html
+cp intro.html lifestyle.html
+cp intro.html conclusion.html
